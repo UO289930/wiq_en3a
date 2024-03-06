98511d649312e4606e248682f4033943393ab685
Alberto-Guerra <albertoguerrarodas@gmail.com>
2024-03-06 16:18:19 +0100

    Alert Dialog Working

--- webapp/src/components/general/Home.tsx
+++ webapp/src/components/general/Home.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Nav } from "./Nav";
 import Game from "../Game/Game";
-import { usePlayingState } from "../../stores/playing-store";
+import { usePlayingState, useShowCancellingDialog } from "../../stores/playing-store";
 import { Button } from "../ui/button";
+import { AlertDialog, AlertDialogPortal, AlertDialogTrigger,AlertDialogOverlay,AlertDialogContent } from "../ui/alert-dialog";
 
 
 
 export const Home = () => {
     const isPlaying = usePlayingState(state => state.playing);
+    const showDialog = useShowCancellingDialog(state => state.show);
 
     return (<div>
         <Nav />
@@ -16,6 +18,19 @@ export const Home = () => {
             <Button onClick={() => {usePlayingState.getState().startPlaying()}}>Start Game</Button>
         </div>
         )}
+    <AlertDialog open={showDialog}>
+      <AlertDialogPortal>
+        <AlertDialogOverlay />
+        <AlertDialogContent>
+            <div>
+                <h1>Are you sure you want to leave? You will lose the progress of the game</h1>
+                <Button onClick={() => {useShowCancellingDialog.getState().functionToExecute(); useShowCancellingDialog.getState().setShow(false)}}>Yes</Button>
+                <Button onClick={() => {useShowCancellingDialog.getState().setShow(false)}}>No</Button>
+            </div>
+        </AlertDialogContent>
+      </AlertDialogPortal>
+    </AlertDialog>
+
         </div>
     );
     }
--- webapp/src/stores/playing-store.ts
+++ webapp/src/stores/playing-store.ts
@@ -13,3 +13,27 @@ export const usePlayingState = create<PlayingState>((set) => ({
     startPlaying: () => set({playing: true}),
     stopPlaying: () => set({playing: false})
   }));
+
+interface ShowCancellingDialog {
+    show: boolean,
+    setShow: (show: boolean) => void,
+    functionToExecute: () => void,
+    setFunctionToExecute: (func: () => void) => void,
+}
+
+export const useShowCancellingDialog = create<ShowCancellingDialog>((set) => ({
+    show: false,
+    setShow: (show: boolean) => set({show: show}),
+    functionToExecute: () => {},
+    setFunctionToExecute: (func: () => void) => set({functionToExecute: func}),
+  }));
+
+export const handleShowDialog = ( func: () => void) =>  {
+    if(usePlayingState.getState().playing){
+      useShowCancellingDialog.getState().setShow(true);
+      useShowCancellingDialog.getState().setFunctionToExecute(func);
+    } else {
+      func();
+    }
+}
+
