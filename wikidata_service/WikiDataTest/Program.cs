1589342dcee4badb7fd0b965a507b6343f0995b9
sergiollende <uo283367@uniovi.es>
2024-03-07 17:56:36 +0100

    WikiData Service revamped

--- wikidata_service/WikiDataTest/Program.cs
+++ wikidata_service/WikiDataTest/Program.cs
@@ -1,3 +1,5 @@
+using WikiDataTest.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(new QueryService());
+builder.Services.AddSingleton(new WikiDataService());
+builder.Services.AddSingleton(new QuestionService());
+
 
 var app = builder.Build();
 
---
+++ wikidata_service/WikiDataTest/Services/QueryService.cs
@@ -0,0 +1,10 @@
+﻿namespace WikiDataTest.Services
+{
+    public class QueryService
+    {
+        public string GetCapitalsQuery()
+        {
+            return "SELECT ?capitalLabel ?countryLabel WHERE {  ?capital wdt:P1376 ?country.  ?capital wdt:P31 wd:Q5119. ?country wdt:P31 wd:Q3624078.  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }} LIMIT 400";
+        }
+    }
+}
