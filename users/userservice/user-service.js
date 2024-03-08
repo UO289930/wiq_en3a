750fb944f943841fe8bd2e026b2d884da8149bd7
ritacatuxo <ritafdezortiz@gmail.com>
2024-03-08 17:10:47 +0100

    /edit request finished

--- users/userservice/user-service.js
+++ users/userservice/user-service.js
@@ -2,29 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const User = require('./user-model') // user model 
+const User = require('./user-model')
 
-
-// Get all users - not working
-router.get('/allUsers', async (req, res) => {
-  try {
-    // Obtener todos los usuarios usando el modelo User
-    const allUsers = await User.find();
-
-    // Objeto JSON con la lista de usuarios
-    const allUsersJSON = {
-      users: allUsers
-    };
-
-    res.json(allUsersJSON);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
 
 
 // GET route to retrieve an specific user by username
-// 'http://localhost:8001/getOneUser?username=nombre_de_usuario'
+// 'http://localhost:8002/getOneUser?username=nombre_de_usuario'
 
 router.get('/getUser', async (req, res) => {
   try {
@@ -44,9 +27,6 @@ router.get('/getUser', async (req, res) => {
           mongoose.connection.close();
         }
       });
-      //const users = await User.find(); // Retrieve all users from the database
-      //console.log("Users:", users); // Print users in the terminal
-      //res.json(users); // Send the array of users as JSON response
   } catch (error) {
       res.status(500).json({ error: 'Internal Server Error' });
   }
@@ -91,6 +71,35 @@ router.post('/adduser', async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message }); 
     }});
+
+
+
+
+// edit a user to update the total and correct question answered
+router.post('/editUser', async (req, res) => {
+  try {
+
+      // --- find the user to be updated
+      const db = mongoose.connection.useDb("UsersDB"); // database
+      const userCollection = db.collection('User');    // collection
+      
+      let userToUpdate = await userCollection.findOne({ username: req.body.username });
+      if (!userToUpdate) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      // --- update the fields 
+      userToUpdate.questions_answered = userToUpdate.questions_answered + req.body.questions_answered;
+      userToUpdate.correctly_answered_questions = userToUpdate.correctly_answered_questions + req.body.correctly_answered_questions;
+
+      // --- update the user in the database
+      await userCollection.updateOne({ username: userToUpdate.username }, { $set: userToUpdate });
+      return res.json({ message: 'User updated' });
+
+  } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 
