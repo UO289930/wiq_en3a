ed7329db13ad219eb59e1eb3660c56138e08c871
sergiollende <uo283367@uniovi.es>
2024-04-15 23:07:39 +0200

    Getting data fast and randomly from wikidata for trivial game

--- wikidataservice/src/Services/QueryGenerator.js
+++ wikidataservice/src/Services/QueryGenerator.js
@@ -3,22 +3,29 @@ const data = require('../Models/Question-Model.json');
 class QueryGenerator {
     static getQuestionsAndQuery(topic, limit = 10) {
         const items = data.array.filter(item => item.topic == topic);
-        const randomIndex = Math.floor(Math.random() * items.length);
-        let itemData = items[randomIndex];
         let query = '';
+        let list = [];
+        
+        // Itererate trough the items and generate a query for each one
+        for (let item of items) {
+            // if ('year' in item.) {
+            //     // If the item has a year property, generate a query with the date parsed
+            //     query = this.generateSparqlQuery(itemData.themeId, itemData.attributeId, limit);
+            //     // query = this.generateSparqlQueryYear(itemData.themeId, itemData.attributeId, limit);
+            // } else {
+            //     query = this.generateSparqlQuery(itemData.themeId, itemData.attributeId, limit);
+            // }
 
-        // If the item has a year property, generate a query with the date parsed
-        if ('year' in itemData) {
-            query = this.generateSparqlQuery(itemData.themeId, itemData.attributeId, limit);
-            // query = this.generateSparqlQueryYear(itemData.themeId, itemData.attributeId, limit);
-        } else {
-            query = this.generateSparqlQuery(itemData.themeId, itemData.attributeId, limit);
+            query = this.generateSparqlQuery(item.themeId, item.attributeId, 5);
+
+            let obj = {
+                query: query,
+                questionText: item.question,
+            };
+            list.push(obj);
         }
 
-        return {
-            query: query,
-            questionText: itemData.question
-        };
+        return list;
     }
 
     // Geography
@@ -34,10 +41,6 @@ class QueryGenerator {
     // Chemistry
     static getElementSymbolQuery(limit = 10) {
         return this.generateSparqlQuery("Q11344", "P246", limit);
-    }
-
-    static getChemicalCompoundAndFormulaQuery(limit = 10) {
-        return this.generateSparqlQuery("Q11173", "P274", limit);
     }
 
     static generateSparqlQuery(themeId, attributeId, limit) {
