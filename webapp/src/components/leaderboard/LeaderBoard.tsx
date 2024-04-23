95ba8a751919f212004096f351eac18522c22b69
ritacatuxo <ritafdezortiz@gmail.com>
2024-04-23 14:10:03 +0200

    leaderboards together

--- webapp/src/components/leaderboard/LeaderBoard.tsx
+++ webapp/src/components/leaderboard/LeaderBoard.tsx
@@ -1,33 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import RankingTable from "./RankingTable"
 import TrivialRankingTable from "./TrivialRankingTable"
 import { TabList, Tab, TabPanels, TabPanel, Tabs } from "@chakra-ui/react"
-
+ 
 
 export default function LeaderBoard() {
 
+    const [tabs, setTabs] = useState<string[]>(["NORMAL", "TRIVIAL"]);
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
 
     return (
         <>
             <h1 className="h1-leaderboard"> LEADERBOARD </h1>
             
-            <Tabs isFitted variant='enclosed'>
-                <TabList mb='1em'>
-                    <Tab>One</Tab>
-                    <Tab>Two</Tab>
+            <Tabs isFitted className="chakra-tabs-container">
+                <TabList className="chakra-tabs" mb='1em'>
+                    {
+                        tabs.map((tab, index) => {
+                            return(
+                                <Tab 
+                                    onClick={() => setSelectedIndex(index)}
+                                    key={index} 
+                                    className={index === selectedIndex ? "chakra-tabs-tab-selected" : "chakra-tabs-tab"}>
+                                        {tab}
+                                </Tab>
+                            )
+                        })
+                    }
                 </TabList>
                 <TabPanels>
                     <TabPanel>
-                    <p>one!</p>
+                        <RankingTable />
                     </TabPanel>
                     <TabPanel>
-                    <p>two!</p>
+                        <TrivialRankingTable />
                     </TabPanel>
                 </TabPanels>
             </Tabs>
 
-            <TrivialRankingTable />
+            
             
 
         </>
