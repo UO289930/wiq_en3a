11134ebab8d1f22a7af0b0597a6a7c401e692591
Sergio Truébano <uo289930@uniovi.es>
2024-04-24 18:19:38 +0200

    All gateway endpoints test

--- gatewayservice/gateway-service.test.js
+++ gatewayservice/gateway-service.test.js
@@ -82,6 +82,37 @@ describe('Gateway Service', () => {
           ],
         }
       });
+    } else if(url.endsWith('getAllUsers')) {
+      return Promise.resolve({
+        status: 200, 
+        data: {
+          users:
+            [{
+              "_id":"65e604d6b9b58650687ee809",
+              "username":"tomas",
+              "password":"$2b$10$cD6x4mUOP9Q16SfMpbWXGugPczuYcV/HMaJbVTWJ.guZoy.LmEe6e",
+              "email":"tomas@gmail.com",
+              "questions_answered":500,
+              "correctly_answered_questions":200,
+              "createdAt":"2024-03-03T17:30:54.385Z",
+              "cheeseCount":1
+            }]
+        }
+      });
+    } else if(url.endsWith('/getUser')){
+      return Promise.resolve({
+        status: 200, 
+        data: {
+          "_id":"65e604d6b9b58650687ee809",
+          "username":"tomas",
+          "password":"$2b$10$cD6x4mUOP9Q16SfMpbWXGugPczuYcV/HMaJbVTWJ.guZoy.LmEe6e",
+          "email":"tomas@gmail.com",
+          "questions_answered":500,
+          "correctly_answered_questions":200,
+          "createdAt":"2024-03-03T17:30:54.385Z",
+          "cheeseCount":1
+        }
+      });
     }
     
 
@@ -99,9 +130,11 @@ describe('Gateway Service', () => {
       return Promise.resolve({ data: { token: 'mockedToken' } });
     } else if (url.endsWith('/adduser')) {
       return Promise.resolve({ data: { userId: 'mockedUserId' } });
-    } else if(url.endsWith('/edituser')){
+    } else if(url.endsWith('/sumNormalStats')){
       return Promise.resolve({ data: { message: 'User updated' } });
-    }
+    } else if(url.endsWith('/sumTrivialStats')){
+      return Promise.resolve({ data: { message: 'User updated' } });
+    } 
   });
 
   // Test health
@@ -198,5 +231,61 @@ describe('Gateway Service', () => {
     const response = await request(app).get("/api-doc");
     expect(response.status).toBe(301);
   });
+
+  // Test /sumStats
+  it('should respond with a successful update operation of normal game mode stats', async () => {
+
+    await sumStats('/sumNormalStats', { 
+      username: 'newuser',
+      questions_answered:500,
+      correctly_answered_questions:200
+    });
+  }, 6000);
+
+  it('should respond with a successful update operation of trivial game mode stats', async () => {
+    await sumStats('/sumTrivialStats', { 
+      username: 'newuser', 
+      cheeseCount:10
+    });
+  }, 6000);
+
+
+  async function sumStats(endpoint, user){
+    const response = await request(app)
+      .post(endpoint)
+      .send(user);
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("User updated");
+  }
+
+  // Test getUser(s)
+
+  it('should retrieve the specified user', async () => {
+    const response = await request(app).post('/getUser').send({username: "tomas"});
+    expect(response.status).toBe(200);
+    console.log(response.body);
+    checkCorrectUserFields(response.body);
+
+  }, 6000);
+
+  it('should retrieve all the users', async () => {
+    const response = await request(app).get('/getAllUsers');
+    expect(response.status).toBe(200);
+    expect(response.body.users.length).toBe(1);
+    console.log(response.body);
+    await checkCorrectUserFields(response.body.users[0]);
+
+  }, 6000);
+
+  async function checkCorrectUserFields(user){
+    expect(user).toHaveProperty("_id");
+    expect(user).toHaveProperty("username");
+    expect(user).toHaveProperty("password");
+    expect(user).toHaveProperty("createdAt");
+    expect(user).toHaveProperty("email");
+    expect(user).toHaveProperty("questions_answered");
+    expect(user).toHaveProperty("correctly_answered_questions");
+    expect(user).toHaveProperty("cheeseCount");
+  }
   
 });
