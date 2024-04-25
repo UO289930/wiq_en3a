d95e0b7d81c0cfa24364b4c5b85f71ed589aeca8
sergiollende <uo283367@uniovi.es>
2024-04-25 14:01:04 +0200

    Change local url mongo

--- users/index.js
+++ users/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8003;
 
 
 // Connect to MongoDB
-const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://prueba:prueba@cluster0.kjzbhst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoUri = process.env.MONGODB_URI;
 
 mongoose.connect(mongoUri).then(
   console.log('Succesfully connected to MongoDB')
