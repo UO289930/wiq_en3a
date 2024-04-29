c1fab0cc16bfbad575e0a81902c1a04a3efef173
sergiollende <uo283367@uniovi.es>
2024-04-29 17:34:41 +0200

    Info page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,6 +3,7 @@ import { loginWithToken } from './services/auth-service';
 import Authentication from './components/auth/Authentication';
 import { useUserStore } from './stores/user-store';
 import { Home } from './components/general/Home';
+import Info from './components/general/Info';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { SimpleNav } from './components/general/SimpleNav';
 import { TriviaGame } from './components/Game/Trivia/TriviaGame';
@@ -36,6 +37,7 @@ function App() {
           <Route path="trivia/hard" element={<TriviaGame difficulty={getHardString()}/>} /> 
           <Route path="/leaderboard" element={<LeaderBoard />}/>
           <Route path="/stats" element={<Statistics />}/>
+          <Route path="/info" element={<Info/>} />
           <Route path="logout" element={<Logout/>} />
           <Route path="*" element={<Home/>} />
 
